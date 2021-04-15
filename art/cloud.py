8e267ac2606b30b3ba2fd04ea8058d308ed8d458
ranasharjeel <sharrq1@gmail.com>
2021-04-15 20:11:03 +0500

    Top genres testing with word cloud impl

--- art/cloud.py
+++ art/cloud.py
@@ -4,24 +4,34 @@ from PIL import Image
 from wordcloud import WordCloud, ImageColorGenerator
 
 
-IMG_PATH = "assets/note.png"
+IMG_PATH = "assets/"
 OUT_PATH = "assets/img.png"
 
-def generateWordCloud(word_list):
+def generateWordCloud(word_list, mask_name, mode):
+    # Edit image path
+    IMG_PATH = "assets/" + mask_name + ".png";
+
     words = Counter(word_list)
     im = Image.open(IMG_PATH).convert("RGB")
     custom_mask = np.array(im)
     img_colors = ImageColorGenerator(custom_mask)
 
-    # Word cloud
     wc = WordCloud(
         background_color='white',
         mask=custom_mask,
         contour_width=10,
         contour_color='black',
         color_func=img_colors
-    ).generate_from_frequencies(words)
+    )
+    
+    # Word cloud generation - MODE 0 (Frequencies)
+    if mode == 0:
+        wc.generate_from_frequencies(words)
 
+
+    # Word cloud - MODE 1 (RAW)
+    elif mode == 1:
+        wc.generate(word_list)
 
     # Output wordcloud to assets
     wc.to_file(OUT_PATH)
--- art/views.py
+++ art/views.py
@@ -12,16 +12,28 @@ def index(request):
     # Spotify data
     sp = spotify_auth.getSpotifyObj()
 
-    # Get top artist of the user 
+    # Get data from user
     top_artists_list = []
+    top_genres_list = []
     if(sp != errors.couldNotAuthenticate()):
-        top_artists = sp.current_user_top_artists(50).get('items')
+        
+        top = sp.current_user_top_artists(200).get('items')
 
-        for i in top_artists:
+        # Top artists
+        for i in top:
             top_artists_list.append(i['name'])
     
-    # Generate word cloud from top artist names
-    cloud.generateWordCloud(top_artists_list)
+
+        # Top genres
+        for i in top:
+            for j in i['genres']:
+                top_genres_list.append(j)
+        
+
+
+    # Generate word cloud from top artist/genres names
+    cloud.generateWordCloud(top_artists_list, "note", 0)
+    #cloud.generateWordCloud(top_genres_list, "note", 0)
 
     return render(request, 'index.html', {'top_artists_list' : top_artists_list})
 
