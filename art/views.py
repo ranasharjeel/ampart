b62db2af5c01b0e146442e041da0989d0df6f5af
ranasharjeel <sharrq1@gmail.com>
2021-04-20 23:32:04 +0500

    Improved mask outlines so word cloud text stops clipping

--- art/views.py
+++ art/views.py
@@ -16,38 +16,9 @@ SCOPE = "user-library-read user-top-read"
 
 # Index page
 def index(request):
-    '''
-    # Spotify data
-    sp = spotify_auth.getSpotifyObj()
-
-    # Get data from user
-    top_artists_list = []
-    top_genres_list = []
-    if(sp != errors.couldNotAuthenticate()):
-        
-        top = sp.current_user_top_artists(200).get('items')
-
-        # Top artists
-        for i in top:
-            top_artists_list.append(i['name'])
-    
-
-        # Top genres
-        for i in top:
-            for j in i['genres']:
-                top_genres_list.append(j)
-        
-
-
-    # Generate word cloud from top artist/genres names
-    cloud.generateWordCloud(top_artists_list, "note", "artists")
-    cloud.generateWordCloud(top_genres_list, "single", "genres")
-    '''
-
-
 
     # Package data to JSON and send to index template
-    # (to get authorization code)
+    # (to get authorization code on login click)
     auth_data = {
         'CLIENT_ID' : CLIENT_ID,
         'CLIENT_SECRET' : CLIENT_SECRET,
